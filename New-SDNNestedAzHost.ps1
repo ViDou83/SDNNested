801557afce55d9e43eabc508e7323cf77865318e
Vincent Douhet <vidou@microsoft.com>
2020-01-23 15:16:45 +0100

    sync

--- New-SDNNestedAzHost.ps1
+++ New-SDNNestedAzHost.ps1
@@ -227,9 +227,9 @@ else
     New-Item -ItemType File -Path $env:temp\injectedscript.ps1  | Out-Null
 
     $Content = "winrm qc /force
-    netsh advfirewall firewall add rule name=WinRMHTTP dir=in action=allow protocol=TCP localport=5985
-    netsh advfirewall firewall add rule name=WinRMHTTPS dir=in action=allow protocol=TCP localport=5986
-    "
+netsh advfirewall firewall add rule name=WinRMHTTP dir=in action=allow protocol=TCP localport=5985
+netsh advfirewall firewall add rule name=WinRMHTTPS dir=in action=allow protocol=TCP localport=5986
+"
 
     Add-Content $env:temp\injectedscript.ps1 $Content  | Out-Null
     
